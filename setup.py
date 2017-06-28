d7f8bdd638af027740318922ad707b36643189f5
Ruben <ruben@adsmurai.com>
2017-06-28 14:08:45 +0200

    Fix versions of flask

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Flask', 'elasticsearch', 'pycurl', 'psycopg2', 'pytz', 'timezonefinder', 'geopy', 'Flask-Cors'],
+    install_requires=['Flask==0.12.2', 'elasticsearch', 'pycurl', 'psycopg2', 'pytz', 'timezonefinder', 'geopy', 'Flask-Cors==3.0.2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
