c55d131090c6868505b77f1547bfbb441a769e34
Ruben <ruben@adsmurai.com>
2017-06-28 14:07:04 +0200

    fix error in flask dependencies

--- setup.py
+++ setup.py
@@ -62,13 +62,12 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    # py_modules=["api-pokemongo-predictor"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['flask', 'elasticsearch', 'pycurl', 'psycopg2', 'pytz', 'timezonefinder', 'geopy', 'flask-cors'],
+    install_requires=['Flask', 'elasticsearch', 'pycurl', 'psycopg2', 'pytz', 'timezonefinder', 'geopy', 'Flask-Cors'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
